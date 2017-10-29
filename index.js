c5be60320b766c8b75e1424c47c3c5aaec65dadf
Joey Cho <cjy12182000@naver.com>
2017-10-29 14:11:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function distanceFromHqInFeet(ending){
 function distanceTravelledInFeet(beginning,ending){
   return 264 * Math.abs(ending - beginning)
 }
+
+function calculatesFarePrice(start, destination){
+  dis = distanceTravelledInFeet(start,destination)
+  if (dis<=400){
+    return 0
+  }
+  else if (400<dis<2000){
+    return 0.02 * dis
+  }
+  else if(2500>=dis>2000){
+    return 25
+  }
+  else{
+    return 'cannot travel that far'
+  }
+  
+}
