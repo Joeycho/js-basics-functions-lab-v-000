078f3ebc45d94a8540107acc78944978bf36d139
Joey Cho <cjy12182000@naver.com>
2017-10-29 14:00:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(arg)
+{
+  return Math.abs(arg-42)
+}
+
+function calculateVertical(beginning, ending){
+  return 264*(ending - beginning)*4
+}
